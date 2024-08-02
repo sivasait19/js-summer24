6894f9476c01169e2be53fdb97c714177aa683db
sivasait19 <stakk@my.bridgeport.edu>
2024-08-02 16:31:46 -0500

    array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+
+//create an array
+const arrA = []
+
+//update an array
+arrA[1] = 999;
+arrA.push()
+console.log(arrA);
+//console.log(arrA[0]);
---
+++ compare.js
@@ -0,0 +1,16 @@
+// compare values
+
+// triple equal sign
+console.log("7" === 7);//
+console.log(7 === 7);
+console.log(8 === 7);
+
+//double equal sign:
+console.log("7" == 7); //
+console.log(7 === 7);
+console.log(8 == 7); 
+
+//
+
+console.log(0 === false);
+console.log(0 == false);
